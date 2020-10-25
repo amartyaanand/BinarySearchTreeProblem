e063fe6d1670f5bdc3d640e72d327033c7177320
Amartya <amartyaanand21@live.com>
2020-10-25 22:29:26 +0530

    [Amartya] Add : Finding Size of Binary Search Tree.

--- BSTProblem/BinarySearchTree.cs
+++ BSTProblem/BinarySearchTree.cs
@@ -21,6 +21,14 @@ namespace BSTProblem
                 treeSize++;
             }
         }
+        public int Size()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Binary Search Tree is Empty");
+            }
+            return treeSize;
+        }
         public void Display()
         {
             if (root == null)
